41feac67861bac09b46171f1c04cb7807fd7a750
Pavlína Baladová <pavlina.baladova@tul.cz>
2025-10-15 09:07:25 +0200

    #4vytvoření základního programu a uložení změn

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d34155d2d081df37c25894e86e6b732d1c54ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251129856e3de3e463439664a8cc40a44df06551")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
